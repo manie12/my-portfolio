d8adf3025c63974b8183cfaeded750e1d3e93207
Jacob Runo <jacobkip254@gmail.com>
2021-02-10 22:37:12 -0800

    fully designed final touches

---
+++ contact.js
@@ -0,0 +1,6 @@
+const list = document.querySelector('.list__type')
+const image = document.querySelector('.img1');
+
+image.addEventListener('click', () => {
+    list.classList.toggle('list__type')
+})
